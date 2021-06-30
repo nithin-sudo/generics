6757a5fc6109c2f7887da6855b76eee8911d1381
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-30 12:16:57 +0530

    Added:initial setup for print maximum of three variables.

---
+++ src/com/bridgelabz/PrintMaximum.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class PrintMaximum {
+    public static void main(String[] args) {
+        System.out.println("Welcome to PrintMaximum of three variables");
+    }
+}
