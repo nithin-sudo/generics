fd165de85366b16a316d5f2ea421537d7dda262c
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-30 13:13:58 +0530

    Added:stringMaximum method and returning maximum variable among three.

--- src/com/bridgelabz/PrintMaximum.java
+++ src/com/bridgelabz/PrintMaximum.java
@@ -1,16 +1,6 @@
 package com.bridgelabz;
 public class PrintMaximum {
     /**
-     * creating three variables of type Integer and calling IntegerMaximum method.
-     * @param args
-     */
-    public static void main(String[] args) {
-        Integer firstNumber=1,secondNumber=2,thirdNumber=3;
-        Float firstFloat=1.11f,secondFloat=2.2f,thirdFloat=3.3f;
-        integerMaximum(firstNumber,secondNumber,thirdNumber);
-        floatMaximum(firstFloat,secondFloat,thirdFloat);
-    }
-    /**
      *Finding maximum integer among three integers using compareTo method.
      * @param x
      * @param y
@@ -31,7 +21,7 @@ public class PrintMaximum {
         return maximum;
     }
     /**
-     * Finding maximum float value among three integers using compareTo method
+     * Finding maximum float value among three float using compareTo method.
      * @param x
      * @param y
      * @param z
@@ -39,6 +29,24 @@ public class PrintMaximum {
      */
     public static Float floatMaximum(Float x,Float y,Float z) {
         Float maximum = x;
+        if (y.compareTo(maximum) > 0) {
+            maximum = y;
+        }
+        if (z.compareTo(maximum) > 0) {
+            maximum = z;
+        }
+        printMax(x, y, z, maximum);
+        return maximum;
+    }
+    /**
+     * Finding maximum String among three strings using compareTo method.
+     * @param x
+     * @param y
+     * @param z
+     * @return
+     */
+    public static String stringMaximum(String x,String y,String z) {
+        String maximum = x;
         if (y.compareTo(maximum) > 0) {
             maximum = y;
         }
@@ -58,5 +66,17 @@ public class PrintMaximum {
      */
     public static <E>void printMax(E x,E y,E z, E max) {
         System.out.println("Maximum of "+x+" ,"+y+" and "+z+"  is "+max);
+    }
+    /**
+     * creating three variables of type Integer and calling IntegerMaximum method.
+     * @param args
+     */
+    public static void main(String[] args) {
+        Integer firstNumber=1,secondNumber=2,thirdNumber=3;
+        Float firstFloat=1.11f,secondFloat=2.2f,thirdFloat=3.3f;
+        String firstString="linux",secondString="windows",thirdString="fedora";
+        integerMaximum(firstNumber,secondNumber,thirdNumber);
+        floatMaximum(firstFloat,secondFloat,thirdFloat);
+        stringMaximum(firstString,secondString,thirdString);
     }
 }
